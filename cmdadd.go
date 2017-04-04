d4e29fa33e9e3189927e6c54fa25aeca6543b763
Alexander Sack <asac@pantacor.com>
2017-04-05 00:47:24 +0200

    land proper error to exit error translation for cmdadd.go

--- cmdadd.go
+++ cmdadd.go
@@ -19,17 +19,17 @@ func CommandAdd() cli.Command {
 		Action: func(c *cli.Context) error {
 			wd, err := os.Getwd()
 			if err != nil {
-				return err
+				return cli.NewExitError(err, 1)
 			}
 
 			pvr, err := NewPvr(c.App, wd)
 			if err != nil {
-				return err
+				return cli.NewExitError(err, 2)
 			}
 
 			err = pvr.AddFile(c.Args())
 			if err != nil {
-				return err
+				return cli.NewExitError(err, 3)
 			}
 
 			return nil
