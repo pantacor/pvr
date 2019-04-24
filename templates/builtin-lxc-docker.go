4f27a8200921f9300a82989e8511f8df731ff722
Alexander Sack <asac@pantacor.com>
2019-04-24 23:57:45 +0200

    add sprig and land initial templating for lxc.container.conf and run.json

--- templates/builtin-lxc-docker.go
+++ templates/builtin-lxc-docker.go
@@ -6,30 +6,43 @@ lxc.tty = 4
 lxc.pts = 1024
 lxc.cgroup.devices.allow = a
 lxc.utsname = major
-lxc.rootfs = overlayfs:/volumes/limescan-mini.squashfs:/storage/limescan-gini.disk.11/upper
-lxc.init_cmd = /bin/systemd
+lxc.rootfs = overlayfs:/volumes/{{- .Source.name -}}/root.squashfs:/volumes/{{- .Source.name -}}/lxc-overlay/upper
+lxc.init.cmd = {{ .Docker.Entrypoint }}
 lxc.mount.auto = proc sys:rw cgroup-full
 lxc.mount.entry = /dev dev none bind,rw,create=dir 0 0
 lxc.mount.entry = /storage pvstorage none bind,rw,create=dir 0 0
 lxc.mount.entry = /etc/resolv.conf etc/resolv.conf none bind,rw,create=file 0 0
-lxc.mount.entry=tmpfs run tmpfs rw,nodev,relatime,mode=755 0 0
-lxc.environment = force_noncontainer=1`
+lxc.mount.entry = tmpfs run tmpfs rw,nodev,relatime,mode=755 0 0
+{{- with $src := .Source -}}
+{{- range $key, $value := $src.persistence -}}
+{{- if ne $key "lxc-overlay" }}
+lxc.mount.entry = /volumes/{{ $src.name }}/docker-{{ $key | replace "/" "-" }} {{ trimPrefix "/" $key }} none bind,rw,create=dir 0 0
+{{- end -}}
+{{- end -}}
+{{- end }}
+lxc.environment = force_noncontainer=1
+`
 
 	RUN_JSON = `
 {
 	"#spec":"service-manifest-run@1",
 	"config":"lxc.container.conf",
-	"name":"limescan-device",
+	"name":"{{- .Source.name -}}",
 	"root-volume":"root.squashfs",
 	"storage":{
-		"lxc-overlay":{
-			"persistence":"revision"
+		{{- range $key, $value := .Source.persistence -}}
+		{{- if ne $key "lxc-overlay" }}
+		"docker-{{ $key | replace "/" "-" -}}": {
+			"persistence": "{{ $value }}"
+		},
+		{{- end -}}
+		{{- end }}
+		"lxc-overlay" : {
+			"persistence": "{{ index .Source.persistence "lxc-overlay" }}"
 		}
 	},
 	"type":"lxc",
-	"volumes":[
-		"something-else.squashfs"
-	]
+	"volumes":[]
 }`
 )
 
