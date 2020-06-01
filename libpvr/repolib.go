0e3cdbf149cab75fbf9d2f6e3b966c9d4dc03864
Alexander (asac) Sack <asac@pantacor.com>
2020-06-01 16:10:03 +0000

    Revert "Making the output of pvr get & pvr merge commands more readable to users"
    This reverts commit 84a133b3e57f2dac071e80c4e259af617526311c

--- libpvr/repolib.go
+++ libpvr/repolib.go
@@ -1492,11 +1492,8 @@ func (p *Pvr) grabObjects(requests ...*grab.Request) error {
 	progressBarSlice := make([]*pb.ProgressBar, 0)
 
 	for _, v := range requests {
-		fileName := v.Label
-		if len(v.Label) >= 15 {
-			fileName = v.Label[:15]
-		}
-		progressBars[v] = pb.New(0).Prefix(fileName)
+		shortFile := filepath.Base(v.Filename)[:15]
+		progressBars[v] = pb.New(0).Prefix(shortFile)
 		progressBars[v].ShowCounters = false
 		progressBars[v].SetUnits(pb.KB)
 		progressBarSlice = append(progressBarSlice, progressBars[v])
@@ -1590,9 +1587,6 @@ err:
 	}
 	t.Stop()
 
-	fmt.Println("\nImported " + strconv.Itoa(completed) + " objects to " + p.Objdir)
-	fmt.Println("\n\nRun pvr checkout to checkout the changed files into the workspace.")
-
 	return nil
 }
 
@@ -1662,7 +1656,6 @@ func (p *Pvr) getObjects(pvrRemote pvrapi.PvrRemote, jsonMap map[string]interfac
 		}
 		req.SkipExisting = true
 		req.Tag = response.Header().Get(objects.HttpHeaderPantahubObjectType)
-		req.Label = remoteObject.ObjectName
 
 		grabs = append(grabs, req)
 	}
